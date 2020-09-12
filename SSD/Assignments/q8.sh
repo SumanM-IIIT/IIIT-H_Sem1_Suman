68172f09cafc5b84b0610f7e29011ce14b0b5191
Suman Mitra <suman.mitra@students.iiit.ac.in>
2020-09-12 20:42:07 +0530

    Modified q8.sh

--- SSD/Assignments/q8.sh
+++ SSD/Assignments/q8.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
 clear
+read line < $1
+declare -a arr
+flag=0
+IFS=' '
+read -ra arr<<<"$line"
+
+num_re="(^[0-9]$)|(^[0-5][0-9]$)|[*]"
+if ! [[ "${arr[0]}" =~ $num_re ]]
+then
+	flag=1
+fi
+
+num_re="(^[0-9]$)|(^[0-1][0-9]$)|(^2[0-3]$)|[*]"
+if ! [[ "${arr[1]}" =~ $num_re ]]
+then
+        flag=1
+fi
+
+num_re="(^[1-9]$)|(^0[1-9]$)|(^[1-2][0-9]$)|^3[0-1]$|[*]"
+if ! [[ "${arr[2]}" =~ $num_re ]]
+then
+        flag=1
+fi
+
+num_re="(^[1-9]$)|(^0[1-9]$)|(^1[0-2]$)|[*]"
+if ! [[ "${arr[3]}" =~ $num_re ]]
+then
+        flag=1
+fi
+
+num_re="(^[0-6]$)|(^0[0-6]$)|[*]"
+if ! [[ "${arr[4]}" =~ $num_re ]]
+then
+        flag=1
+fi
+
+if [[ $flag =~ 1 ]]
+then
+	echo "No"
+else
+	echo "Yes"
+fi
 
